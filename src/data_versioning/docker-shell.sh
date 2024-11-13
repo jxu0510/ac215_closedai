071f791746bc962866e58801c0507ae9646db32f
Yini Mao <ninamao@g.harvard.edu>
2024-11-13 16:35:46 -0500

    track dataset v1"

---
+++ src/data_versioning/docker-shell.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+export BASE_DIR=$(pwd)
+export SECRETS_DIR=$(pwd)/../../secrets/
+export GCS_BUCKET_NAME="closed-ai"
+export GCP_PROJECT="xenon-depth-434717-n0"
+export GCP_ZONE="us-central1-a"
+export GOOGLE_APPLICATION_CREDENTIALS="../../secrets/llm-service-account.json"
+
+echo "Building image"
+docker build -t data-version-cli -f Dockerfile .
+
+echo "Running container"
+docker run --rm --name data-version-cli -ti \
+--privileged \
+--cap-add SYS_ADMIN \
+--device /dev/fuse \
+-v "$BASE_DIR":/app \
+-v "$SECRETS_DIR":/secrets \
+-v ~/.gitconfig:/etc/gitconfig \
+-e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+-e GCP_PROJECT=$GCP_PROJECT \
+-e GCP_ZONE=$GCP_ZONE \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
+
+mkdir -p /app/finetune_dataset
+mkdir -p /app/rag_dataset
